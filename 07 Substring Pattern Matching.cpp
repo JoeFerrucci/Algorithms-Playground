d7aacb54143feefcfd0c0c1c01ea78c9ed336ff5
Joe Ferrucci <mrferrucci@gmail.com>
2014-11-17 12:40:13 -0800

    Solve the substring pattern matching problem.

--- 07 Substring Pattern Matching.cpp
+++ 07 Substring Pattern Matching.cpp
@@ -3,17 +3,38 @@
 using namespace std;
 
 int find_substring(char* pattern, char* text) {
+
     int pattern_len = strlen(pattern);
     int text_len = strlen(text);
 
-    for (int i = 0; i != (text_len - pattern_len); i++) {
-        
+    int j;  // start of match
+
+    for (int i = 0; i <= (text_len - pattern_len); i++) {
+        j = 0;
+
+        while ( (j < pattern_len) && (text[i+j] == pattern[j]) )
+            j++;
+
+        if (j == pattern_len) 
+            return (i);
+
     }
+
+    return (-1);
 
 }
 
 int main(int argc, char const *argv[])
 {
-    /* code */
+    char article[] = "Today is November 17, 2014";
+    char pattern[] = "day";
+
+    int startOfSubstring = find_substring(pattern, article);
+
+    if (startOfSubstring == -1) 
+        cout << "No pattern was matched\n";
+    else
+        cout << "Substring pattern was found at position " << startOfSubstring << "\n";
+
     return 0;
 }
